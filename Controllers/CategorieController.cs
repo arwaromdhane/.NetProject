0a9d4d70b8a260dd74f96757387737a6efdc728e
arwa <arwa@example.com>
2019-11-30 19:22:48 +0100

    init

---
+++ Controllers/CategorieController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace projet_net.Controllers
+{
+    public class CategorieController : Controller
+    {
+        // GET: Categorie
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ajouterCategorie()
+        {
+            return View();
+        }
+    }
+}
